fa73068d0c9a3dc7c4dc2ad09749bbe8d774f0fa
Jocelin Thomas <jocelinthomas177@gmail.com>
2019-01-11 16:09:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/glide_example/SlideshowDialogFragment.java
@@ -0,0 +1,12 @@
+package com.example.user.glide_example;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SlideshowDialogFragment extends DialogFragment {
+
+}
